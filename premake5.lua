cc22001af496d4c00aa56223469c4b1a1c75077a
MaxatHome <mhtwedt@gmail.com>
2023-11-09 22:12:49 -0600

    Update premake5.lua
    Writing correct premake

--- premake5.lua
+++ premake5.lua
@@ -1,50 +1,28 @@
-project "GLFW"
+project "imGui"
   kind "StaticLib"
-  language "C"
+  language "C++"
 
   targetdir ("bin/" .. outputdir .. "/%{prj.name}")
   objdir ("bin-int" .. outputdir .. "/%{prj.name}")
 
   files
   {
-    "include/GLFW/glfw3.h",
-    "include/GLFW/glfw3native.h",
-    "src/glfw_config.h",
-    "src/context.c",
-    "src/init.c",
-    "src/input.c",
-    "src/monitor.c",
-    "src/vulkan.c",
-    "src/window.c"
+    "imconfig.h",
+    "imgui.h",
+    "imgui.cpp",
+    "imgui_draw.cpp",
+    "imgui_internal.h",
+    "imgui_widgets.cpp",
+    "imstb_rectpack.h",
+    "imstb_textedit.h",
+    "imstb_truetype.h",
+    "imgui_demo.cpp"
   }
 
 filter "system:windows"
   systemversion "latest"
+  cppdialect("C++20")
   staticruntime "On"
 
-files
-{
-  "src/win32_init.c",
-  "src/win32_joystick.c",
-  "src/win32_monitor.c",
-  "src/win32_time.c",
-  "src/win32_thread.c",
-  "src/win32_window.c",
-  "src/wgl_context.c",
-  "src/egl_context.c",
-  "src/osmesa_context.c"
-}
-
-defines
-{
-  "_GLFW_WIN32",
-  "_CRT_SECURE_NO_WARNINGS"
-}
-
-filter "configurations:Debug"
-  runtime "Debug"
-  symbols "On"
-
-filter "configurations:Release"
-  runtime "Release"
-  optimize "On"
+filter { "system:windows", "configurations:Release" }
+  buildoptions "/MT"
